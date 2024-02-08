c7a2c46165732a7f41572d14f154fc2be0bdc6a7
snakiso <igorchagov@mail.ru>
2024-02-08 17:19:27 +0300

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {NumericInput} from "./components/NumericInput/NumericInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [numbersForRender, setNumbersForRender] = useState<string[]>([])
+    return (
+        <div className="App">
+            <h2>Введите число и нажмите enter</h2>
+            <NumericInput currentNums={numbersForRender} callback={(num) => setNumbersForRender(num)}/>
+            <div className={'nums'}>
+                <h4>числа которые вы вводили</h4>
+                {numbersForRender.map((num, index) => {
+                    return (
+                        <span key={index}>{num}</span>
+                    )
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/NumericInput/NumericInput.tsx
+++ src/components/NumericInput/NumericInput.tsx
@@ -1,24 +1,58 @@
 import {ChangeEvent, useState} from "react";
+import {keyboardKey} from "@testing-library/user-event";
 
-const NumericInput = () => {
+type NumericInputProps = {
+    callback: (num: string[]) => void
+    currentNums: string[]
+}
+
+export const NumericInput = ({callback, currentNums}: NumericInputProps) => {
     const [value, setValue] = useState('');
+
+    const valueSplitByDot = value.split('.')
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         const inputValue = e.currentTarget.value;
 
-        // Проверка на ввод только цифр с двумя нулями после точки
         const regex = /^\d+(\.\d{0,2})?$/;
         if (inputValue === '' || regex.test(inputValue)) {
             setValue(inputValue);
         }
     };
 
+    const onKeyDownHandler = (e: keyboardKey) => {
+        if (e.key === 'Enter') {
+            if (!value.includes('.') || valueSplitByDot.length === 2) {
+                callback([Number(value).toFixed(2), ...currentNums])
+                setValue('')
+            } else if (valueSplitByDot[1].length === 1) {
+                callback([(value + '0'), ...currentNums])
+                setValue('')
+            } else {
+                callback([value, ...currentNums])
+                setValue('')
+            }
+        }
+    }
+
+    const onBlurHandler = () => {
+        if (value && !value.includes('.')) {
+            setValue(prev => prev + '.00')
+        } else if (value && valueSplitByDot[1].length === 1) {
+            setValue(prev => prev + '0')
+        } else if (valueSplitByDot.length === 2 && valueSplitByDot[1] === '') {
+            setValue(prev => prev + '00')
+        }
+    }
+
     return (
         <input
             type="text"
             value={value}
             onChange={handleInputChange}
-            placeholder="Введите число"
+            onKeyDown={onKeyDownHandler}
+            onBlur={onBlurHandler}
+            placeholder="Введите число типа 0.00"
         />
     );
 };
