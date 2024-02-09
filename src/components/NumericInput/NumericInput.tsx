9e1f7fea5a15b708c5cc54181ba98e249488bc3b
snakiso <igorchagov@mail.ru>
2024-02-09 11:13:18 +0300

    fixed selectionStart

--- src/components/NumericInput/NumericInput.tsx
+++ src/components/NumericInput/NumericInput.tsx
@@ -1,6 +1,5 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useRef, useState} from "react";
 import {NumericFormat} from "react-number-format";
-import {keyboardKey} from "@testing-library/user-event";
 
 type NumericInputProps = {
     callback: (num: string[]) => void
@@ -9,6 +8,8 @@ type NumericInputProps = {
 
 export const NumericInput = ({callback, currentNums}: NumericInputProps) => {
     const [value, setValue] = useState<string>('0.00')
+    const [select, setSelect] = useState<number | null>(null)
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const onBlurHandler = () => {
         if (!value) {
@@ -17,10 +18,19 @@ export const NumericInput = ({callback, currentNums}: NumericInputProps) => {
     }
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.currentTarget.value);
-    };
+        setValue(e.currentTarget.value)
+    }
 
-    const onKeyDownHandler = (e: keyboardKey) => {
+    const onClickHandler = (e: React.MouseEvent<HTMLInputElement>) => {
+        if (inputRef.current?.selectionStart === 0) {
+            setSelect(0)
+        }else{
+            setSelect(null)
+        }
+    }
+
+
+    const onKeyDownHandler = (e: React.KeyboardEvent) => {
         let firstValue = value.split('.')[0]
         if (e.key === "Enter") {
             if (firstValue.length === 1 && firstValue === '0') {
@@ -30,6 +40,20 @@ export const NumericInput = ({callback, currentNums}: NumericInputProps) => {
                 callback([newValue, ...currentNums]);
             }
         }
+        if(e.key === 'ArrowLeft'){
+           if(inputRef.current?.selectionStart === 1 || inputRef.current?.selectionStart === 0){
+               setSelect(0)
+           } else(
+               setSelect(null)
+           )
+        }
+        if(e.key === 'ArrowRight'){
+            setSelect(null)
+        }
+        if (e.key === 'Backspace' && select === 0) {
+            inputRef.current?.setSelectionRange(select,0)
+        }
+
     }
 
     return (
@@ -37,11 +61,15 @@ export const NumericInput = ({callback, currentNums}: NumericInputProps) => {
                        onValueChange={(values) => {
                            setValue(values.value)
                        }}
+                       onClick={onClickHandler}
                        decimalScale={2}
                        fixedDecimalScale
                        onBlur={onBlurHandler}
                        onChange={onChangeHandler}
-                       onKeyDown={onKeyDownHandler}/>
+                       onKeyDown={onKeyDownHandler}
+                       getInputRef={inputRef}
+        />
+
     );
 }
 
